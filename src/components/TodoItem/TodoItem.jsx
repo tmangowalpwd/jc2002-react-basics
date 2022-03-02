7c0f33fe49794821aaccc31d3faf761c3d7632bc
theodevoid <tmangowal@gmail.com>
2022-03-02 10:38:58 +0700

    todo item

--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -2,11 +2,18 @@ import { Card, CardBody, CardTitle, CardText, Button } from "reactstrap";
 
 const TodoItem = () => {
   return (
-    <Card>
+    <Card className="my-2">
       <CardBody>
-        <CardTitle>2 Mar 2022</CardTitle>
-        <CardText>Belajar programming</CardText>
-        <Button color="success">Done</Button>
+        <div className="d-flex justify-content-between">
+          <div>
+            <CardTitle tag="h5" className="fw-bold">
+              2 Mar 2022
+            </CardTitle>
+            <CardText>Belajar programming</CardText>
+          </div>
+          {/* <Button color="success">Done</Button> */}
+          <Button color="danger">On Going</Button>
+        </div>
       </CardBody>
     </Card>
   );
