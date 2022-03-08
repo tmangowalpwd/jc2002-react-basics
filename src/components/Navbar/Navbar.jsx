5266fd05d1c3a9f277bfe2c5d5f7302e949886fc
theodevoid <tmangowal@gmail.com>
2022-03-08 10:19:22 +0700

    react-router-dom

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,21 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
     <nav>
       <div className="link-wrapper">
-        <a href="#home">Home</a>
-        <a href="#band">Band</a>
-        <a href="#tour">Tour</a>
+        <Link to="/" style={{ textDecoration: "none" }}>
+          Home
+        </Link>
+        <Link to="/band">Band</Link>
+        <Link to="/tour">Tour</Link>
         <a href="contact.html">Contact</a>
       </div>
       <div className="search-wrapper">
         <i className="bi bi-search" />
       </div>
     </nav>
-  )
-}
+  );
+};
 
 export default Navbar;
