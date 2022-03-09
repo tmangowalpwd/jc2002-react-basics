736068a191279ba86355d2bc4b632414f43d974d
theodevoid <tmangowal@gmail.com>
2022-03-09 10:39:03 +0700

    product in-memory search

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,19 +1,80 @@
-import { Box, Text, Input, Center, FormLabel } from "@chakra-ui/react";
+import {
+  Box,
+  Text,
+  Input,
+  Center,
+  FormLabel,
+  Flex,
+  Button,
+} from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 import { axiosInstance } from "../configs/api";
 
 const ProductCard = ({ productName, price, category }) => {
   return (
-    <Box borderWidth={1} borderColor="gray" borderRadius="4px" width="150px">
+    <Box
+      margin="2"
+      borderWidth={1}
+      borderColor="gray"
+      borderRadius="4px"
+      width="150px"
+      padding="4"
+    >
       <Text fontSize="large" fontWeight="bold">
-        Product Name
+        {productName}
       </Text>
-      <Text>Rp. 12,000</Text>
-      <Text>Books</Text>
+      <Text>Rp. {price}</Text>
+      <Text>{category}</Text>
     </Box>
   );
 };
 
 const ProductPage = () => {
+  const [productList, setProductList] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+
+  const [searchValue, setSearchValue] = useState("");
+
+  const inputHandler = (event) => {
+    const { value } = event.target;
+
+    setSearchInput(value);
+  };
+
+  const fetchProducts = () => {
+    axiosInstance
+      .get("/products")
+      .then((res) => {
+        setProductList(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("Terjadi kesalahan di server");
+      });
+  };
+
+  const renderProducts = () => {
+    return productList.map((val) => {
+      if (val.product_name.toLowerCase().includes(searchValue.toLowerCase())) {
+        return (
+          <ProductCard
+            category={val.category}
+            price={val.price}
+            productName={val.product_name}
+          />
+        );
+      }
+    });
+  };
+
+  const searchButtonHandler = () => {
+    setSearchValue(searchInput);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
     <Center>
       <Box width="xl">
@@ -22,9 +83,14 @@ const ProductPage = () => {
         </Text>
 
         <FormLabel htmlFor="searchProduct">Product Name</FormLabel>
-        <Input id="searchProduct" />
+        <Input onChange={inputHandler} id="searchProduct" />
+        <Button onClick={searchButtonHandler} marginTop="1">
+          Search
+        </Button>
 
-        <ProductCard />
+        <Flex wrap="wrap" marginTop="4">
+          {renderProducts()}
+        </Flex>
       </Box>
     </Center>
   );
